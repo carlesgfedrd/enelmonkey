22571d571fa2ed0c966fd2a6d3613bc20bfa0ac1
adriansanchezenel <adrian.sanchez.enel@gmail.com>
2026-07-07 11:33:28 +0200

    Petites millores en la visualització de les dates i en el càlcul de la duració dels prerequisits.

--- cuatro/Prerequisits_Gantt.user.js
+++ cuatro/Prerequisits_Gantt.user.js
@@ -5,7 +5,7 @@
 // @match          http*://*.force.com/*
 // @match          http*://*.salesforce.com/*
 // @author         Adrian Sanchez Martinez (adrian.sanchez@enel.com)
-// @version        0.8.6
+// @version        0.8.7
 // ==/UserScript==
 
 (function() {
@@ -130,7 +130,7 @@
         if (i % SaltoFecha === 0) {     // Si el dia actual és un múltiple del salt de dates, mostrem la data a la capçalera
 
             if (SaltoFecha >= 90) { // Si el salt de dates és de 90 dies o més, mostrem només el mes i l'any
-                textoFecha = `${mes}/${any}`;
+                textoFecha = `${dia}${mes}/${any}`;
             } else {
                 textoFecha = `${dia}/${mes}`;
             }
@@ -188,7 +188,8 @@
             if (!ini || !prevista) return;
 
             const offset = Math.floor((ini - minFecha) / 86400000);
-            const fechaFin = real || prevista   // Si hi ha una data real de finalització, l'utilitzem; en cas contrari, utilitzem la data prevista
+            //const fechaFin = real || prevista   // Si hi ha una data real de finalització, l'utilitzem; en cas contrari, utilitzem la data prevista
+            const fechaFin = real || hoyOffset
             const duracion = Math.max( 1, Math.ceil((fechaFin - ini) / 86400000) + 1); // Calculem la duració del prerequisit en dies (mínim 1 dia) i sumem 1 per incloure el dia d'inici i el dia de finalització
             const diasPlanificats = Math.max( 1, Math.ceil((prevista - ini) / 86400000) + 1); // Calculem els dies planificats entre inici i prevista
             const diasReals = real ? Math.max( 1, Math.ceil((real - ini) / 86400000) + 1) : null; // Calculem els dies reals entre inici i data real de finalització (si existeix)
@@ -246,7 +247,7 @@
 
     body{
         font-family:Arial;
-        padding:20px;
+        padding:20px 20px 20px 50px;
     }
 
     h2{
